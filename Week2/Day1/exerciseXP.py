f094e1ec0ae1c683d56e9a2b98eba322906f7fd3
pamellaester <you@example.com>
2023-09-03 14:21:49 +0300

    exerciseXP-W2D1

---
+++ Week2/Day1/exerciseXP.py
@@ -0,0 +1,61 @@
+# exercise 1:
+hello = "Hello world"
+print("{}".format(hello))
+print("{}".format(hello))
+print("{}".format(hello))
+print("{}".format(hello))
+
+# exercise 2:
+print((99^3)*8)
+
+# exercise 3:
+# >>> 5 < 3 = (false)
+# >>> 3 == 3 (true)
+# >>> 3 == "3" = (false)
+# >>> "3" > 3 = (false)
+# >>> "Hello" == "hello" = (true)
+
+# exercise 4:
+computer_brand = "apple"
+print("I have a {} computer".format(computer_brand))
+
+# exercise 5:
+name = "Pamella Ester"
+age = 21
+shoe_size = 37
+info = f"My name is {name}, I'm {age}, my shoe's size is {shoe_size}."
+print(info)
+
+# exercise 6:
+a = 13
+b = 7
+if a >= b:
+    print(f"hello world")
+else:
+    print(f"not hello world")
+
+# exercise 7:
+user_question = int(input("write a number"))
+if (user_question % 2) == 0:
+    print("{0} is Even".format(user_question))
+else:
+    print("{0} is Odd".format(user_question))
+
+# exercise 8:
+my_name = "pamella"
+name_question = input("What's your name? \n")
+if name_question == my_name:
+    print("wow!! The progammer has the same name, what a coincidence!")
+else:
+    print("beautiful name you have!")
+
+#exercise 9:
+
+height_question = float(input("what is your height (inch)? \n"))
+convert_to_cm = height_question*2.54
+if convert_to_cm >=145:
+    print("You are tall enough to ride.")
+elif convert_to_cm < 145:
+    print("You need to grow some more to ride.")  
+
+print("out")  
---
+++ Week2/Day1/main.py
@@ -0,0 +1,41 @@
+# print("hello world")
+# #method used to display some value
+# pet_animal = "cat"
+# #assign a value to the variable
+# #declaring a variable and assigning a value
+# setence = "my favorite is a" + pet_animal  
+
+# #integer
+# year = 2023
+# setence_two = str(year) + "is the current year"
+
+# next_year = year + 1
+
+
+# year = 2023
+# age = 19
+# city = "tel aviv"
+# #string formatting
+# setence_three = f"{year} is the currente year, I'm {age} year old and i live in {city}"
+# print(setence_three)
+
+# first_name = "Pamella Ester"
+# last_name = "Sousa"
+
+# setence_four = f"My full name is {first_name} {last_name}"
+# print(setence_four)
+
+# my_age = 21
+# my_future_age = my_age + 123879
+# sentence_five = f"my future age will be {my_future_age}"
+# print(sentence_five)
+# print("my full name is {} {}".format(first_name,last_name))
+
+sentence = "10 miles"
+question = input("how many miles do you want to convert?\n")
+sentence = sentence.repalce("miles",question)
+print(sentence)
+        
+user_miles = float(input("give me a number of miles"))    
+convert_ti_km = user_miles *1.60934 
+print  
