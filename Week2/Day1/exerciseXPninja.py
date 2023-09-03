d1ca79a47e8a2741c37e7903b2a8e01f03aef459
pamellaester <you@example.com>
2023-09-03 23:46:53 +0300

    exerciseXPninja

---
+++ Week2/Day1/exerciseXPninja.py
@@ -0,0 +1,35 @@
+#Exercise 1:
+
+# pamella@Pamellas-MacBook-Air Day1 % python3 
+
+#Exercise 2:
+
+#exerciseXPninja.py
+# x is True
+# y is False
+# a: 5
+# b: 10
+# pamella@Pamellas-MacBook-Air Day1 % 
+
+# Exercise 3:
+
+# >>> 3 <= 3 < 9
+# >>> 3 == 3 == 3
+# >>> bool(0)    
+# >>> bool(5 == "5")
+# >>> bool(4 == 4) == bool("4" == "4")
+# >>> bool(bool(None))
+# x = (1 == True)
+# y = (1 == False)
+# a = True + 4
+# b = False + 10
+# print("x is", x)
+# print("y is", y)
+# print("a:", a)
+# print("b:", b)
+
+#Exercise 4:
+my_text = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit,sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Duis aute irure dolor an reprehenderit an voluptate velit esse cillum dolore eu fugiat nulla pariatur.Excepteur sint occaecat cupidatat non proident, sunt an culpa qui officia deserunt mollit anim ad est laborum.'
+print(len(my_text))
+
+#Exercise 5:
