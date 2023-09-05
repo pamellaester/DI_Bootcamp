32228108e105ecddedd61458eff6d9ecf3599349
pamellaester <you@example.com>
2023-09-06 02:45:11 +0300

    add more

---
+++ Week2/Day3/exerciseXPninja.py
@@ -0,0 +1,18 @@
+# REverseinp= raw_input()
+
+
+# #write down your logic here 
+
+
+
+# # 
+
+
+# print(reversed) 
+
+
+brand_cars = ["Volkswagen, Toyota, Ford Motor, Honda, Chevrolet"]
+all_brands = 0
+for all_brands in range(len(brand_cars)):
+    brand_cars[all_brands] += all_brands
+print(all_brands)
