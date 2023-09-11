fdbaad1447c2b4dad01073cf5e29d37b017a83a5
pamellaester <you@example.com>
2023-09-11 23:04:35 +0300

    day 1

--- Week3/Day1/dailychallenge.py
+++ Week3/Day1/dailychallenge.py
@@ -1,29 +1,38 @@
 class Farm:
-    def __init__(self,brand):
-        self.name = brand
-
-    def add_animal(self,add_animal,add_number_animal):
-        self.animal = add_animal 
-        self.number = add_number_animal
-        print(str(f"{self.animal} : {self.number}"))
-
-    def add_same_animal(self):
-        self.number = 0
-    for add_number_animal in add_animal > 1:
-     add_number_animal.append()
-        
-
-    def info(self):
-        print("---------------------")
-print("McDonald's farm \n")
+    def __init__(self, name):
+        self.name = name
+        self.list_animal = {}
+    def add_animal(self, animal, amount = 1):
+        if animal in self.list_animal:
+            self.list_animal[animal] += amount
+        else:
+            self.list_animal[animal] = amount
+    def get_info(self):
+        sentence = f"{self.name}'s farm \n\n"
+        for animal, amount in self.list_animal.items():
+           sentence += f"{animal} : {amount} \n"
+        sentence += "\n \n      E-I-E-I-0!"
+        return sentence
+    def get_animal_types(self):
+        list_types = sorted(list(self.list_animal.keys()))
+        return list_types
+    def get_short_info(self):
+        all_types = self.get_animal_types()
+        for index, type in enumerate(all_types):
+            if self.list_animal[type] > 1:
+                all_types[index] += 's'
+        sentence_farm = ", ".join(all_types[0 : -1])
+        print(f"{self.name}'s farm has {sentence_farm} and {all_types[2]}")
 macdonald = Farm("McDonald")
-macdonald.add_animal('cow',5)
+macdonald.add_animal('cow', 5)
 macdonald.add_animal('sheep')
 macdonald.add_animal('sheep')
 macdonald.add_animal('goat', 12)
+print(macdonald.get_info())
+macdonald.get_animal_types()
+macdonald.get_short_info()
 
-print("\n")
-print("---------------------")
+    
         
 
 
--- Week3/Day1/exercisePX.py
+++ Week3/Day1/exercisePX.py
@@ -1,60 +1,60 @@
-#Exercise 1: Cats
+# Exercise 1: Cats
 
-# class Cat:
-#     def __init__(self, cat_name, cat_age):
-#         self.name = cat_name
-#         self.age = cat_age
+class Cat:
+    def __init__(self, cat_name, cat_age):
+        self.name = cat_name
+        self.age = cat_age
 
-#     def show_info(self):
-#         print(f"The dog name is {self.name} and his name is {self.age}")
+    def show_info(self):
+        print(f"The dog name is {self.name} and his name is {self.age}")
 
-# first_cat = Cat("chica",3)
-# second_cat = Cat("lola",6)
-# third_cat = Cat("suzy",9)
+first_cat = Cat("chica",3)
+second_cat = Cat("lola",6)
+third_cat = Cat("suzy",9)
 
 
 
-# show_info(first_cat)
-# show_info(second_cat)
-# show_info(third_cat)
+show_info(first_cat)
+show_info(second_cat)
+show_info(third_cat)
 
-# def longest_age(self):
-#    for cat_age in Cat:
-#     if longest_age = max(self.values())
-# print(f"The oldest cat is {cat_name}, and is {cat_age }years old.")
+def longest_age(self):
+  for cat_age in Cat:
+    if longest_age == max(self.values()):
+     print(f"The oldest cat is {cat_name}, and is {cat_age }years old.")
 
 # Exercise 2 : Dogs
 
-# class Dog:
+class Dog:
    
-#    def __init__(self, name, height):
-#     self.name = name
-#     self.height = height
+   def __init__(self, name, height):
+    self.name = name
+    self.height = height
 
-#    def bark(self):
-#     print(f"{self.name} goes woof!")
+   def bark(self):
+    print(f"{self.name} goes woof!")
 
-#    def jump (self):
-#     print(f"{self.name} jumps {self.height} cm high!”. {self.height} =={self.height}"*2)
+   def jump (self):
+    print(f"{self.name} jumps {self.height} cm high!”. {self.height} =={self.height}"*2)
 
-#    def info(self):
-#      print(f"the dog name is :{self.name} and the height is: {self.height} cm. ")
+   def info(self):
+     print(f"the dog name is :{self.name} and the height is: {self.height} cm. ")
 
-# davids_dog = Dog ("Rex",50)
+davids_dog = Dog ("Rex",50)
 
-# davids_dog.info()
-# davids_dog.bark()
-# davids_dog.jump()
+davids_dog.info()
+davids_dog.bark()
+davids_dog.jump()
 
 # Exercise 3 : Who’s The Song Producer?
 
 class song:
   def __init__(self,lyrics):
-    self.lyrics = [lyrics]
-
+    self.lyrics = lyrics
+  
   def sing_me_a_song(self):
-    new_lyrics = str(self.lyrics).append(",")
-    print(list(new_lyrics))
+    for elem in self.lyrics:
+      print(elem)
 
   
   
@@ -62,7 +62,7 @@ class song:
   
   
   
-stairway = song("Theres a lady who's sure","all that glitters is gold", "and she's buying a stairway to heaven")
+stairway = song(["Theres a lady who's sure","all that glitters is gold", "and she's buying a stairway to heaven"])
     
 stairway.sing_me_a_song()  
 
@@ -72,16 +72,36 @@ stairway.sing_me_a_song()
 class zoo:
   def __init__(self,zoo_name):
     self.name = zoo_name
+    self.animal = ()  
 
-  def add_animal(self,new_animal):
-    animals = []
-    if self.name != new_animal:
-      animals += new_animal
-    return new_animal
+  def add_animal(self,new_animal,amount = 1):
+    if new_animal in self.animal:
+      self.animal[new_animal] += amount
+    else:
+      self.animal[new_animal] = amount
+
     
-  def get_animals(self,animals):
-    print(f"{animals}")
+  def get_animals(self):
+    for new_animals in self.animal:
+     sentence = f"this is all animals in {self.name}: {self.animal}"
+    return sentence
+  
+  def sell_animal(self,animal_sold,amount = 1):
+    if animal_sold in self.animal:
+     self.animal[animal_sold] -= amount
+    else:
+      self.animal[animal_sold] = amount
 
+  def sort_animals(self):
+    list_types = sorted(list(self.animal.keys()))
+    return list_types
+
+  def get_groups (self):
+    if self.animal in list_type:
+      print()
+
+
+ramat_gan_safari = zoo()
   
 
 
